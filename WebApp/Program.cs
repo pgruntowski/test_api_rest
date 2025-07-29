81efd32aad1ba32f73de4266548f2ea2d5650ecf
pgruntowski <p.gruntowski@gmail.com>
2025-07-29 15:35:32 +0200

    update program cs

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<GameDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
